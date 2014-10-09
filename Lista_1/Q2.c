d44bf365795d7d3f5e15352a57320d0c91831559
alexsleite <alexsleite@bol.com.br>
2014-10-09 00:05:40 -0300

    Questão 2

---
+++ Lista_1/Q2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void maior(int *a, int *b){
+int temp;
+temp=*a;
+if(*a<*b){
+*a=*b;
+*b=temp;
+}
+}
+int main(){
+int a,b;
+scanf("%d %d", &a,&b);
+maior(&a,&b);
+printf("%d %d\n\a",a,b);
+return 0;
+}
