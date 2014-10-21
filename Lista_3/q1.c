e377215563ab4f646cc84b1c004b9a5691d85839
alexsleite <alexsleite@bol.com.br>
2014-10-21 00:05:47 -0200

    Questão 01

---
+++ Lista_3/q1.c
@@ -0,0 +1,23 @@
+//Questão 01 da Lista 3.
+//Alexsandro Leite Figueiredo.
+
+#include <stdio.h>
+#include <stdlib.h>
+ 
+int main(void)
+{
+    int *vet,n,a;
+        scanf("%d",&n);
+	vet=malloc(n*sizeof(int));
+    
+	for(a=0; a < x; a++){
+		scanf("%d",&vet[a]);    
+	}
+ 	for(a=0; a < x; a++){
+		printf("%d ",vet[a]);    
+	}
+    
+
+
+	return 0;
+}
